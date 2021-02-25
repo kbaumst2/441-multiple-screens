cbca5ada18fe1e9c8080663636d31c55376de374
Kate Baumstein <kate.baumstein@ibm.com>
2021-02-25 17:38:50 -0500

    cleaned up code

--- TwoViewControllers/TwoViewControllers/SecondViewController.swift
+++ TwoViewControllers/TwoViewControllers/SecondViewController.swift
@@ -23,32 +23,14 @@ class SecondViewController: UIViewController, SendDataToSecondViewProtocol {
     override func viewDidLoad() {
         super.viewDidLoad()
         if(isPenneFavorited2 == false){
-            favoritePenneButton.setImage( UIImage(systemName: "squareshape"), for: .normal)
-            favoritePenneButton.setTitle( "Add to Favorites", for: .normal)
-            favoritePenneButton.backgroundColor = #colorLiteral(red: 0, green: 0.9372810721, blue: 0, alpha: 1)
-            isPenneFavorited2 = false
+            markPenneAsNotFavorited()
         }
         else {
-            favoritePenneButton.setImage( UIImage(systemName: "checkmark.square"), for: .normal)
-            favoritePenneButton.setTitle("Added to favorites", for: .normal)
-            favoritePenneButton.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
-            isPenneFavorited2 = true
+            markPenneAsFavorited()
         }
-        //favoritePenneButton2
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-     
-    */
 
     
     @IBAction func secondMethod(sender: UIButton) {
@@ -57,18 +39,11 @@ class SecondViewController: UIViewController, SendDataToSecondViewProtocol {
     
     @IBAction func clickFavoriteButton(sender: UIButton){
         if(isPenneFavorited2){
-  //      if(favoritePenneButton.currentImage ==  UIImage(systemName: "checkmark.square")){
-            favoritePenneButton.setImage( UIImage(systemName: "squareshape"), for: .normal)
-            favoritePenneButton.setTitle( "Add to Favorites", for: .normal)
-            favoritePenneButton.backgroundColor = #colorLiteral(red: 0, green: 0.9372810721, blue: 0, alpha: 1)
-            isPenneFavorited2 = false
+            markPenneAsNotFavorited()
         }
     else{
      //   else if (favoritePenneButton.currentImage ==  UIImage(systemName: "squareshape")){
-            favoritePenneButton.setImage( UIImage(systemName: "checkmark.square"), for: .normal)
-            favoritePenneButton.setTitle("Added to favorites", for: .normal)
-            favoritePenneButton.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
-            isPenneFavorited2 = true
+            markPenneAsFavorited()
         }
         
         if self.delegate != nil {
@@ -77,22 +52,14 @@ class SecondViewController: UIViewController, SendDataToSecondViewProtocol {
             //dismiss(animated: true, completion: nil)
         }
         
-        
-        
     }
     
     func sendPenneFavoriteInformationToSecondController(isFavorited: Bool) {
         if(isFavorited == true){
-            favoritePenneButton.setImage( UIImage(systemName: "checkmark.square"), for: .normal)
-            favoritePenneButton.setTitle("Added to favorites", for: .normal)
-            favoritePenneButton.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
-            isPenneFavorited2 = true
+            markPenneAsFavorited()
         }
         else {
-            favoritePenneButton.setImage( UIImage(systemName: "squareshape"), for: .normal)
-            favoritePenneButton.setTitle( "Add to Favorites", for: .normal)
-            favoritePenneButton.backgroundColor = #colorLiteral(red: 0, green: 0.9372810721, blue: 0, alpha: 1)
-            isPenneFavorited2 = false
+            markPenneAsNotFavorited()
         }
         
     }
@@ -101,11 +68,23 @@ class SecondViewController: UIViewController, SendDataToSecondViewProtocol {
             if segue.identifier == "penneRecipeSegue" {
                 let firstVC: ViewController = segue.destination as! ViewController
                 //firstVC.isPenneFavorited = isPenneFavorited
-                firstVC.favoritePenneButton.backgroundColor = #colorLiteral(red: 0.003587165615, green: 0.01698361151, blue: 0.005612873472, alpha: 1)
                 firstVC.delegate = self
         }
     }
     
+    func markPenneAsFavorited(){
+        favoritePenneButton.setImage( UIImage(systemName: "checkmark.square"), for: .normal)
+        favoritePenneButton.setTitle("Added to favorites", for: .normal)
+        favoritePenneButton.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+        isPenneFavorited2 = true
+    }
+    
+    func markPenneAsNotFavorited(){
+        favoritePenneButton.setImage( UIImage(systemName: "squareshape"), for: .normal)
+        favoritePenneButton.setTitle( "Add to Favorites", for: .normal)
+        favoritePenneButton.backgroundColor = #colorLiteral(red: 0, green: 0.9372810721, blue: 0, alpha: 1)
+        isPenneFavorited2 = false
+    }
     
     
     
--- TwoViewControllers/TwoViewControllers/ViewController.swift
+++ TwoViewControllers/TwoViewControllers/ViewController.swift
@@ -30,18 +30,10 @@ class ViewController: UIViewController, SendDataProtocol {
     
     @IBAction func clickFavoriteButton(sender: UIButton){
         if(isPenneFavorited){
-        //if(favoritePenneButton.currentImage ==  UIImage(systemName: "checkmark.square")){
-            favoritePenneButton.setImage( UIImage(systemName: "squareshape"), for: .normal)
-            favoritePenneButton.setTitle( "Add to Favorites", for: .normal)
-            favoritePenneButton.backgroundColor = #colorLiteral(red: 0, green: 0.9372810721, blue: 0, alpha: 1)
-            isPenneFavorited = false
+            markPenneAsNotFavorited()
         }
         else{
-   //     else if (favoritePenneButton.currentImage ==  UIImage(systemName: "squareshape")){
-            favoritePenneButton.setImage( UIImage(systemName: "checkmark.square"), for: .normal)
-            favoritePenneButton.setTitle("Added to favorites", for: .normal)
-            favoritePenneButton.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
-            isPenneFavorited = true
+            markPenneAsFavorited()
         }
         print("HERE OUT")
         if self.delegate != nil {
@@ -56,16 +48,10 @@ class ViewController: UIViewController, SendDataProtocol {
     // Delegate Method
     func sendPenneFavoriteInformationToFirstController(isFavorited: Bool) {
             if(isFavorited == true){
-                favoritePenneButton.setImage( UIImage(systemName: "checkmark.square"), for: .normal)
-                favoritePenneButton.setTitle("Added to favorites", for: .normal)
-                favoritePenneButton.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
-                isPenneFavorited = true
+                markPenneAsFavorited()
             }
             else {
-                favoritePenneButton.setImage( UIImage(systemName: "squareshape"), for: .normal)
-                favoritePenneButton.setTitle( "Add to Favorites", for: .normal)
-                favoritePenneButton.backgroundColor = #colorLiteral(red: 0, green: 0.9372810721, blue: 0, alpha: 1)
-                isPenneFavorited = false
+               markPenneAsNotFavorited()
             }
             
         }
@@ -77,6 +63,21 @@ class ViewController: UIViewController, SendDataProtocol {
                 secondVC.delegate = self
             }
         }
+    
+    
+    func markPenneAsFavorited(){
+        favoritePenneButton.setImage( UIImage(systemName: "checkmark.square"), for: .normal)
+        favoritePenneButton.setTitle("Added to favorites", for: .normal)
+        favoritePenneButton.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+        isPenneFavorited = true
+    }
+    
+    func markPenneAsNotFavorited(){
+        favoritePenneButton.setImage( UIImage(systemName: "squareshape"), for: .normal)
+        favoritePenneButton.setTitle( "Add to Favorites", for: .normal)
+        favoritePenneButton.backgroundColor = #colorLiteral(red: 0, green: 0.9372810721, blue: 0, alpha: 1)
+        isPenneFavorited = false
+    }
     
 }
 
